e3b0bbfc4698b3f634d148cc0c7276272966f3ad
Nova <heetask@gmail.com>
2025-08-22 17:53:14 +0900

    35강 실습 - 중간커밋

---
+++ JAVA/35/BungeoBBangTl1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DESKTOP
+ */
+public class BungeoBBangTl1 {
+  int price = 1000;
+  public static void main(String[] args) {
+      BungeoBBangTl1 Bungeo1 = new BungeoBBangTl1();  // BungeoBBangTl의 주소값을 Bungeo1에 저장
+      System.out.println(Bungeo1.price);  // BungeoBBangTl에 있는 price를 가리킴
+  }
+}
---
+++ JAVA/35/BungeoBBangTl2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DESKTOP
+ */
+public class BungeoBBangTl2 {
+  int price = 1000;
+  public static void main(String[] args) {
+    BungeoBBangTl2 Bungeo1 = new BungeoBBangTl2();  // BungeoBBangT2의 주소값을 Bungeo1에 저장
+    Bungeo1.price = 2000;  // BungeoBBangT2에 있는 price를 2000으로 변경
+    System.out.println(Bungeo1.price); // 2000
+  }
+}
